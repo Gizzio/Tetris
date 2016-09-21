7ae5c912a342d442bff4e8174bbc0ce6167271b5
Osjan <wielkibanan8@gmail.com>
2016-09-21 13:58:00 +0200

    Commit

---
+++ Assets/Scripts/FallingElement.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingElement : MonoBehaviour {
+
+    
+    public LayerMask blockingLayer;
+
+
+    private float turnTime;
+    private float timer = 0;
+    private bool fallen = false;
+    
+    void Start()
+    {
+        turnTime = GameMenager.instance.turnTime;
+    }
+
+	void Update ()
+    {
+        if (!fallen)
+        {
+            UpdateTimer();
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && CanMove(Vector2.left))
+            {
+                transform.Translate(-1f, 0, 0);
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow) && CanMove(Vector2.right))
+            {
+                transform.Translate(1f, 0, 0);
+            }
+
+            if (timer > turnTime)
+            {
+                TryToMoveDown();
+            }
+        }
+	}
+
+    void TryToMoveDown()
+    {
+        if(CanMove(Vector2.down))
+        {
+            MoveDown();
+            timer = 0;
+        }
+        else
+        {
+            fallen = true;
+
+           
+            GameMenager.instance.SetNextTurn(this);
+        }
+    }
+
+    void UpdateTimer()
+    {
+        timer += Time.deltaTime;
+    }
+
+    bool CanMove(Vector2 direction)//TODO: naprawić wykrywanie kolizji(czasem blokuje się na rogach)
+    {
+        TurnCollidersOnOff(false);
+        bool isSpace = LineCast(direction);
+        TurnCollidersOnOff(true);
+        return isSpace;
+    }
+
+    void MoveDown()
+    {
+        transform.Translate(0, -1f, 0);
+    }
+
+    void TurnCollidersOnOff(bool state)
+    {
+        BoxCollider2D[] colliders = GetComponentsInChildren<BoxCollider2D>();
+        foreach (BoxCollider2D boxCollider in colliders)
+        {
+            boxCollider.enabled = state;
+        }
+    }
+    
+
+    bool LineCast(Vector2 direction)
+    {
+        Transform[] positions=GetComponentsInChildren<Transform>();
+        foreach(Transform trans in positions)
+        {
+            Vector2 start = trans.position;
+            Vector2 end = start + direction;
+            RaycastHit2D hit = Physics2D.Linecast(start, end, blockingLayer);
+            if(hit.transform!=null)
+            {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+    void SetTurnTime(float time)
+    {
+        turnTime = time;
+    }
+
+}
---
+++ Assets/Scripts/GameMenager.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMenager : MonoBehaviour {
+
+    
+    public static GameMenager instance = null;
+    public float startY = 5.0f;
+    public Object[] blocks;
+    public float turnTime= 3f;
+    public float turnAcceleration = 3f;
+    /*TODO: 
+                -punktację
+                -wykrywanie linii
+                -ściany
+    */
+
+    private FallingElement blockCallingNextTurn = null;
+    private bool isGameOver = true;
+
+    void Awake()//Implementacja singletona
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if(instance!=this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+	void Start ()
+    {
+        SpawnNewBlock();
+	}
+
+    void Update()
+    {
+        if(blockCallingNextTurn!=null)
+        {
+            NextTurn();
+        }
+    }
+
+    public void NextTurn()
+    {
+        blockCallingNextTurn.transform.DetachChildren();
+        Destroy(blockCallingNextTurn.gameObject);
+        blockCallingNextTurn = null;
+
+        ChceckLines();
+        ChceckIfGameOver();
+        if(!isGameOver)
+            SpawnNewBlock();
+    }
+
+    private void ChceckLines()
+    {
+        ///TODO
+    }
+
+    private void ChceckIfGameOver()
+    {
+        //TODO
+    }
+
+    public void SpawnNewBlock()
+    {
+        Vector3 startPoint = new Vector3(0, startY, 0);
+        Instantiate(blocks[Random.Range(0, 4)], startPoint, Quaternion.identity);
+        decreaseTurnTime();
+        
+    }
+
+    private void decreaseTurnTime()
+    {
+        turnTime = turnTime - (turnTime / turnAcceleration);
+    }
+
+    public void SetNextTurn(FallingElement elementCalling)
+    {
+        blockCallingNextTurn = elementCalling;
+    }
+
+    
+}
